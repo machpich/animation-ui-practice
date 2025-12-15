aeae3aee873a313389270aacf632fe9081edab47
obama shoko <shoko.obama@kufu.co.jp>
2025-12-15 18:52:19 +0900

    npm install hover-effect

--- src/pages/animation/animation-1.tsx
+++ src/pages/animation/animation-1.tsx
@@ -3,8 +3,8 @@ import styles from './animation-1.module.css'
 function Animation1() {
   return (
     <div className={styles.container}>
-      <h1>Animation 1</h1>
-      <p>This is the Animation page. You can add your animation content here.</p>
+      <h1>hover effect</h1>
+      <p>https://github.com/robin-dela/hover-effect</p>
     </div>
   )
 }
